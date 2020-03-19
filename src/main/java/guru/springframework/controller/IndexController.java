6d458483ca8332cf9651db004f34a4ff7b5e7f68
DodinD1 <dandodin@yahoo.com>
2020-03-19 10:56:31 -0700

    add lombok

--- src/main/java/guru/springframework/controller/IndexController.java
+++ src/main/java/guru/springframework/controller/IndexController.java
@@ -1,11 +1,13 @@
 package guru.springframework.controller;
 
 import guru.springframework.services.RecipeService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@Slf4j
 public class IndexController {
 
     private RecipeService recipeService;
