d5ed43a0fae8fc0d34f4aa6f83a2a8234c36a190
DodinD1 <dandodin@yahoo.com>
2020-03-19 12:57:59 -0700

    add tests

---
+++ src/test/java/guru/springframework/services/RecipeServiceImplTest.java
@@ -0,0 +1,40 @@
+package guru.springframework.services;
+
+import guru.springframework.domain.Recipe;
+import guru.springframework.respositories.RecipeRepository;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.Assert.assertEquals;
+
+public class RecipeServiceImplTest {
+
+    RecipeService recipeService;
+    @Mock
+    RecipeRepository recipeRepository;
+
+    @Before
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+        recipeService = new RecipeServiceImpl(recipeRepository);
+    }
+
+    @Test
+    public void getRecipes() {
+        Recipe recipe = new Recipe();
+        Set<Recipe> recipeSet = new HashSet<>();
+        recipeSet.add(recipe);
+
+        Mockito.when(recipeRepository.findAll()).thenReturn(recipeSet);
+
+        Set<Recipe> recipes = recipeService.getRecipes();
+        assertEquals(1, recipes.size());
+        Mockito.verify(recipeRepository, Mockito.times(1)).findAll();
+    }
+}
